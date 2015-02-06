7facd2adf728ec076c2e8346e1e0df5cfec31328
Julie Kent <jkent2910@gmail.com>
2015-02-06 17:16:16 +0000

    created controller models views for posts categories

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+ class PostsController < ApplicationController
+   def index
+     @posts = Post.order(updated_at: :desc).limit(25)
+   end
+
+   def show
+     @post = Post.find(params[:id])
+   end
+
+   def new
+     @post = Post.new
+   end
+
+   def create
+     @post = Post.new(post_params)
+     if @post.save
+       redirect_to @post
+     else
+       render "new"
+     end
+   end
+   
+
+  private
+   def post_params
+     params.require(:post).permit(:title, :content, :category_ids => [])
+   end
+
+ end
