9b056015cf2263138b9c43509e17a15cef71a2ed
Julie Kent <jkent2910@gmail.com>
2015-02-06 22:10:00 +0000

    added admin via devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,30 @@
  class PostsController < ApplicationController
    
+     before_action :authenticate_admin!, except: [:index, :show]
+   
+     before_action :ensure_post_ownership, only: [:edit, :update, :destroy]
+   
    def index
      @posts = Post.order(updated_at: :desc).limit(25)
    end
 
    def show
      @post = Post.find(params[:id])
+     
    end
 
    def new
-     @post = Post.new
+     @post = current_admin.posts.build
    end
 
-   def create
-     @post = Post.new(post_params)
+   def create 
+     @post = current_admin.posts.build(post_params)
+     
      if @post.save
-       redirect_to @post
+       redirect_to @post, notice: "Your post has been created!"
      else
        render "new"
-     end
+     end    
    end
    
  	def edit
@@ -35,11 +41,25 @@
 		end
 	end
    
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
 
+		redirect_to posts_path
+	end
+   
+   
   private
    
    def post_params
      params.require(:post).permit(:title, :content, :category_ids => [])
    end
 
+  def ensure_post_ownership
+    if current_admin != Post.find(params[:id]).admin
+      redirect_to root_path, "You do not have access to do perform that action"
+    end
+  end
+   
+   
  end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  belongs_to :admin
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
