8076651593f91c44e48707fa694b7bb905c51d1c
Julie Kent <jkent2910@gmail.com>
2015-02-12 18:31:54 +0000

    styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,12 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :ratings
-  has_attached_file :image, styles: { :medium => "400x400#", :thumb => "100x100#" }
+  has_attached_file :image, 
+  :styles => {
+      :thumb => "100x100#",
+      :small  => "150x150>",
+      :medium => "200x200" }
+
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :title, :content, :categories, presence: true 
 
