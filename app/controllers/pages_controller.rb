92dd43e8517f31c276a966d31e4f78dbd0c6cf7c
Julie Kent <jkent2910@gmail.com>
2015-02-13 18:40:50 +0000

    added story

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def course
+  end
+
+  def story
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :admins
   root 'welcome#index'
   
+  get '/course' => 'pages#course'
+  get '/story' => 'pages#story'
+  
   resources :posts do
     resources :comments
     resources :ratings
